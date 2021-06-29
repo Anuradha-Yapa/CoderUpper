2e29edb5d233c58dba1cf96df8ae56fb7bb3f84a
AnuradhaYapa95 <AnuradhaYapa95@users.github.com>
2021-06-29 21:03:40 +0530

    Dropdown

--- WindowsFormsApp1/AssigningGrade.cs
+++ WindowsFormsApp1/AssigningGrade.cs
@@ -37,8 +37,29 @@ namespace WindowsFormsApp1
 
         private void bunifuThinButton24_Click(object sender, EventArgs e)
         {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\Project\WindowsFormsApp1\DatabaseCP.mdf;Integrated Security=True");
+            con.Open();
 
+            if (bunifuMaterialTextbox3.Text != "")
+            {
+                SqlCommand cmd = new SqlCommand("SELECT ID, Name, Degree, Year, Semester from Student where ID = @ID", con);
 
+                cmd.Parameters.AddWithValue("@ID", int.Parse(bunifuMaterialTextbox3.Text));
+                SqlDataReader da = cmd.ExecuteReader();
+                while (da.Read())
+                {
+                    bunifuMaterialTextbox1.Text = da.GetValue(1).ToString();
+                    bunifuMaterialTextbox2.Text = da.GetValue(2).ToString();
+
+                    string YEAR = da.GetValue(3).ToString();
+                    string SEM = da.GetValue(4).ToString();
+
+                    bunifuDropdown3.Text = YEAR;
+                    bunifuDropdown1.Text = SEM;
+                }
+                con.Close();
+
+            }
         }
     }
 }
