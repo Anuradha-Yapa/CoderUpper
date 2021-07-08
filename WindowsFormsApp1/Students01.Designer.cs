8442163bf8bd9ba3b1351b8bc60c4e5325c4ba7a
AnuradhaYapa95 <AnuradhaYapa95@users.github.com>
2021-07-08 13:34:33 +0530

    Post- Student Amendments

--- WindowsFormsApp1/Students01.Designer.cs
+++ WindowsFormsApp1/Students01.Designer.cs
@@ -45,7 +45,7 @@ namespace WindowsFormsApp1
             this.label13.AutoSize = true;
             this.label13.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label13.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.label13.Location = new System.Drawing.Point(205, 114);
+            this.label13.Location = new System.Drawing.Point(203, 62);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(307, 33);
             this.label13.TabIndex = 155;
