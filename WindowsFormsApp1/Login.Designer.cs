d7387d385f4a88471c40868e5c447cb29ebccea9
AnuradhaYapa95 <AnuradhaYapa95@users.github.com>
2021-07-20 13:39:04 +0530

    Modification 4

--- WindowsFormsApp1/Login.Designer.cs
+++ WindowsFormsApp1/Login.Designer.cs
@@ -80,17 +80,17 @@ namespace WindowsFormsApp1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(165, 305);
+            this.label1.Location = new System.Drawing.Point(132, 306);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 23);
+            this.label1.Size = new System.Drawing.Size(103, 23);
             this.label1.TabIndex = 221;
-            this.label1.Text = "Admin";
+            this.label1.Text = "Username";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(139, 351);
+            this.label2.Location = new System.Drawing.Point(139, 353);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(96, 23);
             this.label2.TabIndex = 222;
